2234aaf62f7732a351af25657d007def1b566307
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-08-07 18:21:33 +0100

    aula de py

---
+++ holla_word.py
@@ -0,0 +1,5 @@
+print(1)
+#este codigo é de teste
+print('Ola mundo')
+print("Muito bem")
+#print("2");
---
+++ variaveis.py
@@ -0,0 +1,26 @@
+# Criar uma variavel simples
+
+#x = 12
+#print(x)
+
+#pergunta = 14
+
+#resposta = 12
+#num_1 = 2
+#NUM_1 = 1
+#print(num_1 )
+#print(NUM_1)
+
+# erro em declaração de variaveis
+
+# 1nunmero = 1
+
+#print(1nunmero)
+
+#nuemro_grande = 1
+#print(nuemro_grande)
+
+
+# String
+nome = 'Belmiro'
+print('Ola tudo bem', nome)
